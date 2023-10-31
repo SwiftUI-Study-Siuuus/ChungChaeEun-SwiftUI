1652778a786eb90a45fb6bfcdf43c8a26a36a8db
chaentopia <cce523@hanmail.net>
2023-10-31 16:13:40 +0900

    Setting 프로젝트 세팅

---
+++ SwiftUI_Weather/SwiftUI_Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Weather
+//
+//  Created by 정채은 on 10/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Weather/SwiftUI_Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI_Weather
+//
+//  Created by 정채은 on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
